289e206bde1acc7c5f718aca56f590b52775fd31
jipf <1170113120@qq.com>
2022-11-25 08:59:28 +0800

    部门问题及import问题

--- customs-web/src/views/system/dept/dialogDept.vue
+++ customs-web/src/views/system/dept/dialogDept.vue
@@ -8,28 +8,7 @@
           <el-form-item label="部门名称" prop="deptName">
             <el-input v-model="formData.deptName" placeholder="请输入部门名称" style="width: 200px"/>
           </el-form-item>
-          <el-form-item label="所在区县" prop="area">
-            <!-- <el-input v-model="formData.area" placeholder="请输入区县" style="width: 200px"/> -->
-            <el-select v-model="formData.areaId"   clearable placeholder="请输入区县" @change="setStreet" style="width: 200px">
-              <el-option
-                v-for="item in areaOptions"
-                :key="item.id"
-                :label="item.areaName"
-                :value="item.id"
-              />
-            </el-select>
-          </el-form-item>
-          <el-form-item label="所在街道" prop="street">
-            <!-- <el-input v-model="formData.street" placeholder="请输入街道(可选)" style="width: 200px"/> -->
-            <el-select v-model="formData.streetId"  placeholder="请输入街道(可选)" style="width: 200px">
-              <el-option
-                v-for="item in streetOptions"
-                :key="item.id"
-                :label="item.streetName"
-                :value="item.id"
-              />
-            </el-select>
-          </el-form-item>
+
           <el-form-item label="显示排序" prop="orderNum">
             <el-input-number v-model="formData.orderNum" controls-position="right" :min="0" />
           </el-form-item>
@@ -48,8 +27,6 @@
 import {dialogTy} from '~/dialog' //引入变量类型
 import {PropType} from 'vue'
 import {getDept} from "@/api/dept";
-import {getArea} from "@/api/area";
-import {getStreetByArea} from "@/api/street";
 
 const propsDept = {
   multiple: false,
@@ -80,8 +57,6 @@ const rules = ref({
 
 const allDept = ref(null)
 const refDeptForm: any = ref(null)
-const areaOptions = ref([])
-const streetOptions = ref([])
 
 const openFun = () => {
   getDept().then((response=>{
@@ -93,9 +68,7 @@ const openFun = () => {
     allDept.value = deptBack
   }))
 
-  getArea().then((response=>{
-    areaOptions.value = response.data
-  }))
+
 
 }
 
@@ -120,11 +93,7 @@ const resetForm = () => {
   refDeptForm.value.resetFields()
 }
 
-const setStreet=(val) => {
-  getStreetByArea(val).then((response=>{
-    streetOptions.value = response.data
-  }))
-}
+
 
 </script>
 
